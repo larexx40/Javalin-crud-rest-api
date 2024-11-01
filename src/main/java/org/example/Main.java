c5d8e6c7494a1b430061d40509b19467e925f6ed
larexx40 <larexx40@gmail.com>
2024-11-01 06:47:13 +0100

    setup app and users in main for restful api

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import io.javalin.Javalin;
+import org.example.controller.UserController;
+import org.example.service.UserService;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,75 +17,10 @@ public class Main {
                 .get("/", ctx -> ctx.result("Hello World"))
                 .start(7070);
 
-        app.get("/welcome/{name}", ctx -> {
-            String name = ctx.pathParam("name");
-
-            //create json response object
-            Map<String, Object> response = new HashMap<>();
-            response.put("status", "success");
-            response.put("name", name);
-            response.put("message", "Hello " + name);
-
-            //return response;
-            ctx.json(response);
-        });
-
-        app.post("/signup", ctx->{
-            // Create the response structure
-            Map<String, Object> response = new HashMap<>();
-
-            try {
-                User user = ctx.bodyAsClass(User.class);
-
-                //validate user
-                List<String> validationErrors = userInputValidator(user);
-                if (!validationErrors.isEmpty()) {
-                    ctx.status(400);
-                    response.put("status", "error");
-                    response.put("message", "Invalid data submitted");
-                    response.put("data", null);
-                    response.put("errors", validationErrors);
-                }else {
-                    response.put("status", "success");
-                    response.put("message", "Data submitted");
-                    response.put("data", user);
-                }
-
-            }catch (Exception e){
-                ctx.status(400);
-                response.put("status", "error");
-                response.put("message", "Missing or invalid data");
-                response.put("data", null);
-            }
-            ctx.json(response);
-        });
+        UserService userService = new UserService();
+        UserController userController = new UserController(userService);
+        userController.userRoutes(app);
     }
 
-    public static class  User{
-        public String name;
-        public String email;
-        public String password;
-        public String username;
 
-    }
-
-    public static List<String> userInputValidator(User user){
-        List<String> errors = new ArrayList<>();
-        if(user.name == null || user.name.isEmpty()){
-            errors.add( "Name is required");
-        }
-        if(user.email == null || user.email.isEmpty()){
-            errors.add( "Email is required");
-        }
-        if(user.password == null || user.password.isEmpty()){
-            errors.add( "Password is required");
-        } else if (user.password.length() < 8) {
-            errors.add( "Password must be at least 8 characters");
-        }
-
-        if(user.username == null || user.username.isEmpty()){
-            errors.add( "Username is required");
-        }
-        return errors;
-    }
 }
