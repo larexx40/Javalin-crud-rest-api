9587b320fdd6c120757dac06283c56c35e965196
larexx40 <larexx40@gmail.com>
2024-11-01 06:10:00 +0100

    DOne with user service

---
+++ src/main/java/org/example/service/UserService.java
@@ -0,0 +1,44 @@
+package org.example.service;
+
+import org.example.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+    private final List<User> users = new ArrayList<>();
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public boolean updateUser(String id ,User newUser) {
+        Optional<User> existingUser = users.stream().filter(user -> user.getId().equals(id)).findFirst();
+        if (existingUser.isPresent()) {
+            User user = existingUser.get();
+            user.setName(newUser.getName());
+            user.setEmail(newUser.getEmail());
+            user.setPassword(newUser.getPassword());
+//            users.add(user);
+            return true;
+        }
+        return false;
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+
+    public boolean deleteUserById(String id){
+        return users.removeIf(user -> user.getId().equals(id));
+    }
+
+    public Optional<User> getUserById(String id){
+        return users.stream().filter(user -> user.getId().equals(id)).findFirst();
+    }
+}
