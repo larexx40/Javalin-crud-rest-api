ffbed8e80339baa5e11f15429425c0c6880bb6fa
larexx40 <larexx40@gmail.com>
2024-11-07 11:43:29 +0100

    setup db config using fluent jdbc

---
+++ src/main/java/org/example/DBConfig.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.codejargon.fluentjdbc.api.FluentJdbc;
+import org.codejargon.fluentjdbc.api.FluentJdbcBuilder;
+
+import javax.sql.DataSource;
+
+public class DBConfig {
+    private static HikariDataSource dataSource;
+    private static FluentJdbc fluentJdbc;
+
+    static {
+        // Load environment variables
+        Dotenv dotenv = Dotenv.load();
+
+        // Configure HikariCP
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dotenv.get("DB_URL"));
+        config.setUsername(dotenv.get("DB_USER"));
+        config.setPassword(dotenv.get("DB_PASSWORD"));
+        config.setMaximumPoolSize(Integer.parseInt(dotenv.get("DB_POOL_SIZE")));
+
+        dataSource = new HikariDataSource(config);
+
+        // Initialize FluentJdbc with the DataSource
+        fluentJdbc = new FluentJdbcBuilder().connectionProvider(dataSource).build();
+    }
+
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public static FluentJdbc getFluentJdbc() {
+        return fluentJdbc;
+    }
+}
