f523e685bdbf9f87e785455db2ba8b95faaa6dde
larexx40 <larexx40@gmail.com>
2024-11-09 03:21:08 +0100

    setup and define custom response class

---
+++ src/main/java/org/example/response/CustomResponse.java
@@ -0,0 +1,49 @@
+package org.example.response;
+
+import java.util.List;
+
+public class CustomResponse {
+    private boolean status;
+    private String message;
+    private Object data;
+    private List<String> error;
+
+    // Constructor for a successful response
+    public CustomResponse(boolean status, String message, Object data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    // Constructor for an error response
+    public CustomResponse(boolean status, String message, List<String> error) {
+        this.status = status;
+        this.message = message;
+        this.error = error;
+    }
+
+    public static CustomResponse success(String message, Object data) {
+        return new CustomResponse(true, message, data);
+    }
+
+    public static CustomResponse error(String message, List<String> error) {
+        return new CustomResponse(false, message, error);
+    }
+
+    // Getters
+    public boolean isStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public List<String> getError() {
+        return error;
+    }
+}
